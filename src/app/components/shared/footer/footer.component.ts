0ea87fdd61a1f0df5dd97adeb35d2d3c57b4cd5a
JessicaSCN <jmnunez@scn.com.uy>
2020-02-20 12:47:11 -0300

    footer design

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -8,71 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   constructor() {}
 
-  // "src/styles.scss",
-  // "node_modules/slick-carousel/slick/slick.scss",
-  // "node_modules/slick-carousel/slick/slick-theme.scss"
 
-  // title = 'ngSlick';
-
-  // slides = [
-  //   { img: 'assets/img/marcas/acer.jpg' },
-  //   { img: 'assets/img/marcas/dell2.jpg' },
-  //   { img: 'assets/img/marcas/hp2.jpg' },
-  //   { img: 'assets/img/marcas/microsoft.jpg' },
-  //   { img: 'assets/img/marcas/iphone.jpg' },
-  //   { img: 'assets/img/marca/nikon.png' },
-  //   { img: 'assets/img/marcas/nokia.jpg' },
-  //   { img: 'assets/img/marcas/samsung.jpg' },
-  //   { img: 'assets/img/marcas/sony.png' },
-  //   { img: 'assets/img/marcas/xiaomi.jpg' },
-  //   { img: 'assets/img/marcas/android.jpg' }
-  // ];
-
-  // slideConfig = {
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  //   nextArrow: '<div class=\'nav-btn next-slide\'></div>',
-  //   prevArrow: '<div class=\'nav-btn prev-slide\'></div>',
-  //   dots: true,
-  //   infinite: false
-  // };
-
-  // addSlide() {
-  //   this.slides.push({ img: 'assets/img/marcas/sony.png' });
-  // }
-
-  // removeSlide() {
-  //   this.slides.length = this.slides.length - 1;
-  // }
-
-  // slickInit(e) {
-  //   console.log('slick initialized');
-  // }
-
-  // breakpoint(e) {
-  //   console.log('breakpoint');
-  // }
-
-  // afterChange(e) {
-  //   console.log('afterChange');
-  // }
-
-  // beforeChange(e) {
-  //   console.log('beforeChange');
-  // }
 
   ngOnInit() {
-    // tslint:disable-next-line: only-arrow-functions
-    $(function() {
-      ($('.your-class') as any).slick({ arrows: false });
-      // tslint:disable-next-line: only-arrow-functions
-      ($('#next') as any).click(function() {
-        ($('.your-class') as any).slick('slickNext');
-      });
-      // tslint:disable-next-line: only-arrow-functions
-      ($('#prev')as any).click(function() {
-        ($('.your-class') as any).slick('slickPrev');
-      });
-    });
+
   }
 }
